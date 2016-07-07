e9463be39fcab890744e1fde02813245a6ec38d5
zhaoName <zhao1529835@126.com>
2016-07-07 14:11:40 +0800

    Initial Commit

---
+++ MulpitleTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MulpitleTableView
+//
+//  Created by zhao on 16/7/7.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
